b9809ab791abbf4afe3dfbc428c393c690883709
seraermolenko <sera.ermolenko@gmail.com>
2024-09-26 21:03:35 -0700

    restored

--- Bloom/settings.py
+++ Bloom/settings.py
@@ -79,14 +79,13 @@ WSGI_APPLICATION = 'Bloom.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = { 'default': 
-{ 'ENGINE': 'django.db.backends.postgresql', # or your chosen database 
-'NAME': os.getenv('DB_NAME'), 
-'USER': os.getenv('DB_USER'), 
-'PASSWORD': os.getenv('DB_PASSWORD'), 
-'HOST': os.getenv('DB_HOST'), 
-'PORT': os.getenv('DB_PORT'), }
+    { 'ENGINE': 'django.db.backends.postgresql', 
+    'NAME': os.getenv('DB_NAME'), 
+    'USER': os.getenv('DB_USER'), 
+    'PASSWORD': os.getenv('DB_PASSWORD'), 
+    'HOST': os.getenv('DB_HOST'), 
+    'PORT': os.getenv('DB_PORT'), }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
